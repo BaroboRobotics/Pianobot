e319fb51787b20a5eac9551ccde38369d849f2e0
Harris Hancock <vortrab@gmail.com>
2014-05-28 17:07:27 -0700

    Fix bug if an object is classless

--- js/jquery-svg-class.js
+++ js/jquery-svg-class.js
@@ -3,20 +3,29 @@
 
 (function ($) {
   $.fn.addClassSvg = function (klass) {
-    return this.attr("class", this.attr("class") + " " + klass);
+    if (typeof this.attr("class") != undefined) {
+      return this.attr("class", this.attr("class") + " " + klass);
+    }
+    return this.attr("class", klass);
   };
 
   $.fn.removeClassSvg = function (klass) {
-    var classes = this.attr("class").split(' ');
-    return this.attr("class", classes.filter(function (e) {
-      return e != klass;
-    }).join(' '));
+    if (typeof this.attr("class") != undefined) {
+      var classes = this.attr("class").split(' ');
+      return this.attr("class", classes.filter(function (e) {
+        return e != klass;
+      }).join(' '));
+    }
+    return this;
   };
 
   $.fn.hasClassSvg = function (klass) {
-    return this.attr("class").split(' ').some(function (e) {
-      return e == klass;
-    });
+    if (typeof this.attr("class") != undefined) {
+      return this.attr("class").split(' ').some(function (e) {
+        return e == klass;
+      });
+    }
+    return false;
   }
 
 })(jQuery);
